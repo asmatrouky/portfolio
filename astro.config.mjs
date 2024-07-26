b627f7e33affbcf1106bcea095f73e99564165e1
Asma Trouky <asmatrouky@MBP-de-Asma.lan>
2024-07-27 01:58:07 +0200

    Configure GitHub Actions for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'http://localhost:8888/portfolio/',
-})
+  site: 'https://portfolio.webmaagency.com',
+});
